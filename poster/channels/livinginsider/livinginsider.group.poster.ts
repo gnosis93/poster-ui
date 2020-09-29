51ed111013244a8126c6d5cfe7e88a4352cf0ad8
mkmardel <mkmardel@gmail.com>
2020-09-29 19:11:00 +0200

    livinginsider login changes

--- poster/channels/livinginsider/livinginsider.group.poster.ts
+++ poster/channels/livinginsider/livinginsider.group.poster.ts
@@ -41,8 +41,8 @@ export class LivinginsiderPoster extends ChannelBase implements IChannel {
         let { username, password } = this.getCredentials();
 
         await loginPage.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
-        await loginPage.goto(this.channelUrl, { waitUntil: 'networkidle2' });
-        await loginPage.click('#none_login_zone');
+        await loginPage.goto(this.channelUrl, { waitUntil: 'load', timeout: 120000 }); //its ok for me now
+        await this.clickTickboxByIndex(loginPage, 1, 'a[data-target="#loginModal"]');
         await loginPage.type('#login_username', username);
         await loginPage.type('#password', password);
         await loginPage.click('#btn-signin');
